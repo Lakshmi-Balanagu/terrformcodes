52375c402f6157cb14f3aafca1a91fda2d516699
Lakshmi-Balanagu <lakshmi.balanagu12@gmail.com>
2025-06-13 22:57:51 +0530

    Update variables.tf

--- scenario2/vpc/variables.tf
+++ scenario2/vpc/variables.tf
@@ -1,22 +1,32 @@
 variable "region" {
-    
+  type        = string
+  description = "AWS region"
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "Name tag for the VPC"
 }
 
 variable "vpc_cidr" {
-    
+  type        = string
+  description = "CIDR block for the VPC"
 }
-variable "vpc_name" {
-    
-}
+
 variable "public_subnet_cidrs" {
-    
+  type        = list(string)
+  description = "List of public subnet CIDRs"
+  default     = []
 }
+
 variable "private_subnet_cidrs" {
-    
+  type        = list(string)
+  description = "List of private subnet CIDRs"
+  default     = []
 }
-variable "" {
-    
-}
-variable "" {
-    
+
+variable "enable_nat_gateway" {
+  type        = bool
+  default     = false
+  description = "Enable NAT Gateway for private subnets"
 }
